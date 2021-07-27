feb387c110c4d20d32be5cd45ac1cc34213ae710
LushnikP <pasha134134@mail,ru>
2021-07-27 22:51:08 +0600

    Initial commit

---
+++ core/src/com/mygdx/game/StarGame.java
@@ -0,0 +1,34 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class StarGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	TextureRegion region;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+		region = new TextureRegion(img, 25,25,100,50);
+	}
+
+	@Override
+	public void render () {
+		ScreenUtils.clear(1, 0, 0, 1);
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
