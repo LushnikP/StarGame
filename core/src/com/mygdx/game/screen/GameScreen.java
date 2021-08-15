399c2f495b2f2fe224178be3e0791dc3a9a9975f
LushnikP <pasha134134@mail,ru>
2021-08-15 22:20:39 +0600

    hw4

--- core/src/com/mygdx/game/screen/GameScreen.java
+++ core/src/com/mygdx/game/screen/GameScreen.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.math.Vector2;
 import com.mygdx.game.base.BaseScreen;
 import com.mygdx.game.math.Rect;
 import com.mygdx.game.sprite.Background;
+import com.mygdx.game.sprite.MainShip;
 import com.mygdx.game.sprite.Star;
 
 public class GameScreen extends BaseScreen {
@@ -18,6 +19,7 @@ public class GameScreen extends BaseScreen {
     private TextureAtlas atlas;
 
     private Star[] stars;
+    private MainShip mainShip;
 
     @Override
     public void show() {
@@ -25,11 +27,12 @@ public class GameScreen extends BaseScreen {
         bg = new Texture("textures/bg.png");
         background = new Background(bg);
 
-        atlas = new TextureAtlas("textures/menuAtlas.tpack");
+        atlas = new TextureAtlas("textures/mainAtlas.tpack");
         stars = new Star[STAR_COUNT];
         for (int i = 0; i < stars.length; i++){
             stars[i] = new Star(atlas);
         }
+        mainShip = new MainShip(atlas);
     }
 
     @Override
@@ -46,6 +49,7 @@ public class GameScreen extends BaseScreen {
         for(Star star : stars){
             star.resize(worldBounds);
         }
+        mainShip.resize(worldBounds);
     }
 
     @Override
@@ -57,11 +61,13 @@ public class GameScreen extends BaseScreen {
 
     @Override
     public boolean touchDown(Vector2 touch, int pointer, int button) {
+        mainShip.touchDown(touch, pointer, button);
         return false;
     }
 
     @Override
     public boolean touchUp(Vector2 touch, int pointer, int button) {
+        mainShip.touchUp(touch, pointer, button);
         return false;
     }
 
@@ -69,6 +75,7 @@ public class GameScreen extends BaseScreen {
         for(Star star : stars){
             star.update(delta);
         }
+        mainShip.update(delta);
     }
     private void draw(){
         batch.begin();
@@ -76,6 +83,19 @@ public class GameScreen extends BaseScreen {
         for(Star star : stars){
             star.draw(batch);
         }
+        mainShip.draw(batch);
         batch.end();
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        mainShip.keyDown(keycode);
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        mainShip.keyUp(keycode);
+        return false;
     }
 }
