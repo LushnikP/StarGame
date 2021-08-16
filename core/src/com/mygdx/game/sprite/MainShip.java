3ede5d32114e06a0c3a5790ab9bce43afc27b1d8
LushnikP <pasha134134@mail,ru>
2021-08-16 15:10:04 +0600

    hw5

--- core/src/com/mygdx/game/sprite/MainShip.java
+++ core/src/com/mygdx/game/sprite/MainShip.java
@@ -1,6 +1,7 @@
 package com.mygdx.game.sprite;
 
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
@@ -13,6 +14,7 @@ public class MainShip extends Sprite {
     private static final float HEIGHT = 0.15f;
     private static final float BUTTON_MARGIN = 0.05f;
     private static final int INVALID_POINTER = -1;
+    private static final float RELOAD_INTERVAL = 0.2f;
 
     private static final float PADDING = 0.01f;
 
@@ -32,22 +34,33 @@ public class MainShip extends Sprite {
     private Vector2 bulletV;
     private float bulletHeight;
     private int bulletDamage;
+    private Sound bulletSound;
+
+    private float reloadInterval;
+    private float reloadTimer;
 
 
-    public MainShip(TextureAtlas atlas, BulletPool bulletPool) {
+    public MainShip(TextureAtlas atlas, BulletPool bulletPool, Sound bulletSound) {
         super(atlas.findRegion("main_ship"), 1, 2, 2);
         this.bulletPool = bulletPool;
+        this.bulletSound = bulletSound;
         bulletRegion = atlas.findRegion("bulletMainShip");
-        bulletPos = new Vector2();;
+        bulletPos = new Vector2();
         bulletV = new Vector2(0, 0.5f);
         bulletHeight = 0.01f;
         bulletDamage = 1;
+        reloadInterval = RELOAD_INTERVAL;
     }
 
     @Override
     public void update(float delta) {
         super.update(delta);
         pos.mulAdd(v, delta);
+        reloadTimer += delta;
+        if(reloadTimer >= reloadInterval){
+            reloadTimer = 0f;
+            shoot();
+        }
         if((getRight() + PADDING) > worldBounce.getRight()){
             setRight(worldBounce.getRight() - PADDING);
             stop();
@@ -56,6 +69,7 @@ public class MainShip extends Sprite {
             setLeft(worldBounce.getLeft() + PADDING);
             stop();
         }
+
     }
 
     @Override
@@ -117,9 +131,6 @@ public class MainShip extends Sprite {
                 pressedRight = true;
                 moveRight();
                 break;
-            case Input.Keys.UP:
-                shoot();
-                break;
         }
         return false;
     }
@@ -160,5 +171,6 @@ public class MainShip extends Sprite {
         Bullet bullet = bulletPool.obtain();
         bulletPos.set(pos.x, pos.y + getHalfHeight());
         bullet.set(this, bulletRegion, bulletPos, bulletV, bulletHeight, worldBounce, bulletDamage);
+        bulletSound.play();
     }
 }
